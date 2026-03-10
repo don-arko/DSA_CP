5676b9dee55da57831b07b60fbde116049e741dc
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-03-10 16:44:45 +0530

    Update gcd.cpp

--- Learned Implementation /gcd.cpp
+++ Learned Implementation /gcd.cpp
@@ -1,5 +1,22 @@
-//Recursive Implementation of GCD 
+//Recursive Implementation of GCD  , Time Complexiy: O(log(min(a,b)))
+
 int gcd(int a,int b){
     if(b==0) return a;
     else return gcd(b,a%b);
+}
+
+
+//One Liner , Time Complexiy: O(log(min(a,b)))
+
+int gcd(int a,int b){
+    return (b)?gcd(b,a%b):a;
+}
+
+//Iterative Implementation of GCD , Time Complexity : 
+int gcd(int a,int b){
+    while(b){
+        a%=b;
+        swap(a,b);
+    }
+    return a;
 }
