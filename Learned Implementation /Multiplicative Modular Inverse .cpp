2c0b31e2b5c0cea89c5bb918483cc53c5fc671f7
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-03-11 14:35:13 +0530

    Add multiplicative modular inverse implementation
    Implement multiplicative modular inverse using Fermat's theorem.

---
+++ Learned Implementation /Multiplicative Modular Inverse .cpp
@@ -0,0 +1,13 @@
+int binpow(int a,int b,int m){
+    a%=m;
+    if(b==0) return 1;
+    if(b&1) return a*binpow(a,b-1,m)%m;
+    else{
+        int temp=binpow(a,b/2,m)%m;
+        return temp*temp%m;
+}
+
+//From Fermat's Little Theorem , if p is prime and a is in modular domain of p i.e. a not divisible by b then a^(p-1) is congruent to 1(mod p) -> a inv=a^(p-2)
+int inverse(int a,int m){
+    return binpow(a,m-2,m);   // calculates (a inv mod m)
+}
