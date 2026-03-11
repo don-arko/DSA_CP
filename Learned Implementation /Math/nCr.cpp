f6314c39cf2f0146ad87ab80837680d0af5ab5f6
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-03-11 22:34:17 +0530

    Create nCr.cpp

---
+++ Learned Implementation /Math/nCr.cpp
@@ -0,0 +1,14 @@
+// Calculates nCr % MOD for n<=10^6 and ofc r<=n 
+//Time Complexity : O(rlog(MOD))
+#define int long long int
+const int MOD
+int nCr(int n,int r){             //O(r.log(MOD))
+    if(r>n) return 0;    //nCr=0 ; r>n
+    r=min(r,n-r);       // optimisation as nCr=nCn-r
+    int res=1;
+    for(int i=0;i<r;i++){
+        res=(res*(n-i))%MOD;
+        res=res*(binpow(i+1,MOD-2,MOD))%MOD; //= (res*(binpow(i+1,MOD-2,MOD)))%MOD  as associativity is Left to right
+    }
+    return res;
+}
