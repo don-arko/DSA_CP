8d7c2a389e0fae417b9057a0785810d8a3bb4095
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-03-11 22:48:23 +0530

    Create nCr with Precomputed factorials.cpp

---
+++ Learned Implementation /Math/nCr with Precomputed factorials.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+const int MOD=1e9+7;
+const int MAX=1e6;
+
+int fact[MAX+1],inv_fact[MAX+1];
+
+int binpow(int a,int b,int m){             //O(log b)=O(log(MAX))
+    a%=m;
+    if(b==0) return 1;
+    if(b&1) return a*binpow(a,b-1,m)%m;    //=  (a*binpow(a,b-1,m))%m as associativity is left to right
+    else {
+        int temp=binpow(a,b/2,m)%m;       //asso : left to right 
+        return temp*temp%m;               //asso : left to right 
+    }   
+}
+
+void factorials(){                        //O(MAX)
+    fact[0]=1;
+    for(int i=1;i<=MAX;i++){
+        fact[i]=i*fact[i-1]%MOD; //asso : left to right and the product <=1e15 hence wont overflow
+    }
+    inv_fact[MAX]=binpow(fact[MAX],MOD-2,MOD);
+    for(int i=MAX-1;i>=0;--i){
+        inv_fact[i]=(i+1)*inv_fact[i+1]%MOD;        //asso: left to right and the produce <=1e15 hence wont overflow
+    }
+}
+
+int nCr(int n,int r){   //O(1) using precomputed factorials and their inverses
+    if(r>n) return 0;
+    return fact[n]*(inv_fact[r]*inv_fact[n-r]%MOD)%MOD;    //= (fact[n]*((inv_fact[r]*inv_fact[n-r])%MOD))%MOD  as Asso: left to right
+}
+
+signed main(){                  //O(T+MAX)
+    int t;cin>>t;
+    factorials();               //O(MAX)
+    while(t--){             
+        int n,r;cin>>n>>r;
+        cout<<nCr(n,r)<<'\n';   //O(1)
+    }
+}
