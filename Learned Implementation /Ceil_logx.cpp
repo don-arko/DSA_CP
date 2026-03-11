d64461334882d41406c2ecf81a1f175363b62e71
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-03-11 06:03:08 +0530

    Refactor ceil_log function to use k instead of cnt

--- Learned Implementation /Ceil_logx.cpp
+++ Learned Implementation /Ceil_logx.cpp
@@ -2,8 +2,8 @@
 //Def of Ceil(log x): min k ; 2^k>=x
 
 int ceil_log(int n){
-    int cnt=0;
-    for(int i=1;i<n;i*=2){
-        cnt++;
-    }return cnt;
+    int k=0;
+    for(int i=1;i<n;i*=2){ // here  i=2^k is checked if i <x it is incremented 
+        k++;
+    }return k;
 }
