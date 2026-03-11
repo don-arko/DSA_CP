04f428e73d9dbd6daf1b2ad09095093b14793a56
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-03-11 12:38:40 +0530

    Update bin_pow.cpp

--- Learned Implementation /bin_pow.cpp
+++ Learned Implementation /bin_pow.cpp
@@ -1,14 +1,11 @@
 //Modular Binary Exponentiation : Time Complexity = O(b) ; b is the exp
 
-int binpow(int a,int b,int mod){
-    a%=mod;
-    if(b==0){
-        return 1;
-    }
-    if(b&1){
-        return a*binpow(a,b-1,mod)%mod;
-    }else {
-        int temp=binpow(a,b/2,mod)%mod;
-        return temp*temp%mod;
+int binpow(int a,int b,int m){
+    a%=m;
+    if(b==0) return 1;
+    if(b&1) return a*binpow(a,b-1,m)%m;
+    else {
+        int temp=binpow(a,b/2,m)%m;
+        return (temp*temp)%m;
     }
 }
