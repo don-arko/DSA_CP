53f939a384c9f689a43cebe0ab019cd759ab3e9f
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-03-18 13:12:28 +0530

    Update Multiplicative Modular Inverse .cpp

--- Learned Implementation /Math/Multiplicative Modular Inverse .cpp
+++ Learned Implementation /Math/Multiplicative Modular Inverse .cpp
@@ -7,7 +7,8 @@ int binpow(int a,int b,int m){
         return temp*temp%m;
 }
 
-//From Fermat's Little Theorem , if p is prime and a is in modular domain of p i.e. a not divisible by b then a^(p-1) is congruent to 1(mod p) -> a inv=a^(p-2)
+//From Fermat's Little Theorem , if p is prime and a is not congruent to 0 in mod domain of p i.e. a not divisible by b then 
+//a^(p-1) is congruent to 1(mod p) => inv(a) =a^(p-2)
 int inverse(int a,int m){
     return binpow(a,m-2,m);   // calculates (a inv mod m)
 }
