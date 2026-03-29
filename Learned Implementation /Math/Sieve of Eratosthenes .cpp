c1d7cb9dc05aebbaa84d2cca6d18e9e02afc2d1f
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-03-29 18:38:47 +0530

    Update Sieve of Eratosthenes .cpp

--- Learned Implementation /Math/Sieve of Eratosthenes .cpp
+++ Learned Implementation /Math/Sieve of Eratosthenes .cpp
@@ -12,3 +12,31 @@ void sieve(){
         }
     }
 }
+
+//Optimised Sieve Code 
+vector<int> sieve(int n){
+    vector<bool> is_prime(n+1,true);
+    is_prime[0]=is_prime[1]=false;
+    for(int i=2;i*i<=n;i++){
+        if(is_prime[i]){
+            for(int j=i*i;j<=n;j+=i){
+                is_prime[j]=false;
+            }
+        }
+    }return is_prime;
+}
+
+//Returning set of primes <=N {Unoptimsed}
+vector<int> sieve(int n){
+    vector<int> primes;
+    vector<bool> is_prime(n+1,true);
+    is_prime[0]=is_prime[1]=false;
+    for(int i=2;i<=n;i++){ //loop till n is necessary as loop till root n would only give as primes till sqrt n but we want primes till n
+        if(is_prime[i]){
+            for(int j=i*i;j++;j+=i){
+                primes.push_back(i);
+                is_prime[j]=false;
+            }
+        }
+    }
+}
