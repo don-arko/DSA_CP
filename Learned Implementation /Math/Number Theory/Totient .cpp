d2a667e9eb212b6bb5871d3041795ecfa5d9288a
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-03-31 18:17:16 +0530

    Create Totient .cpp

---
+++ Learned Implementation /Math/Number Theory/Totient .cpp
@@ -0,0 +1,21 @@
+vector<pair<int,int>> pfact(int x){            //O(rootX)
+    vector<pair<int,int>> ans;
+    for(int i=2;i*i<=x;i++){
+        if(x%i==0){
+            int cnt=0;
+            while(x%i==0){
+                x/=i;
+                cnt++;
+            }ans.push_back({cnt,i});
+        }
+    }if(x>1)ans.push_back({x,1});
+    return ans;
+}
+
+int totient(int x){                            //O(rootX+logX)=O(sqrtX)
+    vector<pair<int,int>> prime=pfact(x);
+     //no need to handle x=1 as empty vector is returned in such case and nothing is subtracted from x
+    for(auto p:prime){
+        x-=x/p.first;
+    }return x;
+}
