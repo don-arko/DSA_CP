ac68cd0353b9ce339100f50071330df5d6712230
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-04-01 07:26:24 +0530

    Create Highest exp of x in N! .cpp

---
+++ Learned Implementation /Math/Number Theory/Number Theoretic Functions/Highest exp of x in N! .cpp
@@ -0,0 +1,26 @@
+vector<pair<int,int>> pfact(int x){
+    vector<pair<int,int>> ans;
+    for(int i=2;i*i<=x;++i){
+        if(x%i==0){
+            int cnt=0;
+            while(x%i==0){
+                x/=i;
+                cnt++;
+            }ans.push_back({i,cnt});
+        }
+    }if(x>1) ans.push_back({x,1});
+    return ans;
+}
+int legendre(int p,int n){  //O(log n)  base is p
+    int ans=0;
+    for(int i=p;i<=n;i*=p){
+        ans+=p/i;
+    }
+}
+int power(int x,int n){            //O(sqrt(x)+logx.logn)
+    vector<pair<int,int>> prime=pfact(x);    //O(sqrtx)
+    int mini=INT_MAX;
+    for(auto p:prime){                        //O(logx.logn)
+        mini=min(mini,legendre(p.first,n)/p.second);
+    }return mini;
+}
