59a54a1822c6dc8e1105394c70cf388cd94b7914
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-04-03 12:08:21 +0530

    Create nCr .cpp

---
+++ Learned Implementation /Math/Combinatorics/nCr .cpp
@@ -0,0 +1,12 @@
+//Time complexity : O(r)
+
+int nCr(int n,int r){
+    if(r<0 || r>n) return 0;
+    r=min(r,n-r);
+    int ans=1; //nC0
+    //calculate nC1,nC2,...,nCr using Recurrence Relation C(n,i+1)=C(n,i)* (n-1)/(i+1) 
+    for(int i=0;i<r;i++){
+        ans=ans*(n-i)/(i+1); //Numerator is always divisible by denominator {Theorem : product of x consecutive nos is always divisble by x!}
+                            //Because RHs is nothing but an binomial coeff which is always an integer
+    }return ans;
+}
