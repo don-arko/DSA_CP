f1aaa230d1f4c087c8a8cd9a4014a87bcc23bcc8
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-04-04 10:08:33 +0530

    Update Row of Pascal's Triangle.cpp

--- Learned Implementation /Math/Combinatorics/Row of Pascal's Triangle.cpp
+++ Learned Implementation /Math/Combinatorics/Row of Pascal's Triangle.cpp
@@ -1,26 +1,16 @@
 /*
 A Row of Pascal's Triangle : nC0  nC1  .......  nCn
 
-Recurrence Relation : nCr=nC(r-1) * (n-r+1)/r
+Recurrence Relation : C(n,i+1)=C(n,i)*(n-i)/(i+1); //we can start calculating from nC1 using nC0
 */
 
 
 //Implementation 1
 int n;cin>>n;
-int term=1; //nC0=1
-cout<<term<<" ";                 //printing the first term
-for(int r=1;r<=n;r++){           //printting the rem terms
-    term=term*(n-r+1)/r;         //do not use the shorthand term*=(n-r+1)/r here division happens first which means we get floor division and result is incorrect
-    cout<<term<<" ";
-}
-
-//Implementation 2
-int n;cin>>n;
-int term=1; //nC0=1
-for(int r=1;r<=n;r++){
-    cout<<term<<" ";
-    //update ans use r+1 in the rec relation  as r for next term is r+1
-    //(n-(r+1)+1)/(r+1) =(n-r)/(r+1)
-    term=term*(n-r)/(r+1);         //do not use the shorthand term*=(n-r+1)/r here division happens first which means we get floor division and result is incorrect
-    
+int ans=1; //nC0=1
+cout<<ans<<" ";                 //printing nC0
+//calculates nC1,nC1,....nCn
+for(int i=0;i<n;i++){           //printting the rem terms
+    ans=(ans*(n-i))/(i+1);        //calculates C(n,i+1)
+    cout<<ans<<'\n';
 }
