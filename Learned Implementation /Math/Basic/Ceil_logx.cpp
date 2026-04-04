76b21a9f3b066ebab8ecee69b733f17904db8994
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-04-04 07:57:00 +0530

    Update Ceil_logx.cpp

--- Learned Implementation /Math/Basic/Ceil_logx.cpp
+++ Learned Implementation /Math/Basic/Ceil_logx.cpp
@@ -1,9 +1,23 @@
-//calculates the ceil of log base2 of (n) : 
-//Def of Ceil(log x): min k ; 2^k>=x
-
+//Time Complexity : O(logx) 
+ 
+//Definition : ⌈logx⌉ = min{k | 2^k>=x}
 int ceil_log(int n){
     int k=0;
     for(int i=1;i<n;i*=2){ // here  i=2^k is checked if i <x it is incremented 
         k++;
-    }return k;
+    }
+    //loop fails at 2^k>=k which is the mean k req
+    return k;
+}
+
+
+
+//Definition : ⌊logx⌋ = max{k | 2^k <=x}
+int floor_log(int x){
+    int k = 0;
+    while((1 << (k + 1)) <= x){
+        k++;
+    }
+    //loop fails at 2(k+1) >k which means 2^k <= x is the max k
+    return k;
 }
