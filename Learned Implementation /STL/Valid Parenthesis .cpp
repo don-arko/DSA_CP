ee08cb520f3307077cde7d29f31609b3924481f0
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-04-10 20:26:36 +0530

    Update Valid Parenthesis .cpp

--- Learned Implementation /STL/Valid Parenthesis .cpp
+++ Learned Implementation /STL/Valid Parenthesis .cpp
@@ -52,24 +52,3 @@ int cnt_min(string s){
     ans+=d;
     return ans;
 }
-void solve(){
-    string s;cin>>s;
-    int d=0;
-    int ans=0;
-    for(char ch:s){
-        if(ch=='(') d++;
-        else d--;
-        if(d<0){
-            ans++;d=0;
-        }
-    }
-    if(d!=0)ans+=abs(d);
-    cout<<ans<<'\n';
-}
-int32_t main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int tt;cin>>tt;
-    while(tt--){
-        solve();
-    }
-}
