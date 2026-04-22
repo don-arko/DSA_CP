e12c8ba94e92b1651ec385b3aeddf627926763db
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-04-22 07:17:58 +0530

    Update Anagram cost ∀ Sliding Window .cpp

--- Learned Implementation /STL/Sliding Window/Anagram /Anagram cost ∀ Sliding Window .cpp
+++ Learned Implementation /STL/Sliding Window/Anagram /Anagram cost ∀ Sliding Window .cpp
@@ -150,25 +150,25 @@ Time Complexity : O(N)
 */
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> const_freq(26,0); //freq array for the chars in string t
+vector<int> b(26,0); //freq array for the chars in string t
 
 
 struct Ds{
     //Data structure
-    int freq[26]={0}; // vector<int> freq(26,0) won't work inside struct
+    int a[26]={0}; // vector<int> freq(26,0) won't work inside struct
     int cost=0; //maintains the curr cost
     //behaves as freq array for every m len window of string S
 
     //methods
     void insert(char ch){    //O(1)
-        cost-=max(0,freq[ch-'a']-const_freq[ch-'a']); //remove the prev contribution of ch from prev ans
+        cost-=max(0,a[ch-'a']-b[ch-'a']); //remove the prev contribution of ch from prev ans
         freq[ch-'a']++;                               //update the freq of ch
-        cost+=max(0,freq[ch-'a']-const_freq[ch-'a']); //add the new contribution to ans
+        cost+=max(0,a[ch-'a']-b[ch-'a']); //add the new contribution to ans
     }
     void erase(char ch){     //O(1)
-        cost-=max(0,freq[ch-'a']-const_freq[ch-'a']); //remove the prev contribution of ch from prev ans
+        cost-=max(0,a[ch-'a']-b[ch-'a']); //remove the prev contribution of ch from prev ans
         freq[ch-'a']--;                               //update the freq of ch
-        cost+=max(0,freq[ch-'a']-const_freq[ch-'a']); //add the new contribution to ans
+        cost+=max(0,a[ch-'a']-b[ch-'a']); //add the new contribution to ans
     }
     int getcost(){          //O(1)
         return cost;
@@ -183,7 +183,7 @@ void solve(){
     
     //build freq array for T
     for(char ch:t){
-        const_freq[ch-'a']++;
+        b[ch-'a']++;
     }
 
   
