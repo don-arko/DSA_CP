6cc6d3669f0465e0bb1f4fe1bee69c0f1f99ca4d
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-04-22 07:21:31 +0530

    Update Anagram Cost  .cpp

--- Learned Implementation /STL/DS Design/Anagram Cost  .cpp
+++ Learned Implementation /STL/DS Design/Anagram Cost  .cpp
@@ -1,20 +1,20 @@
 //Time Complexity : O(1)  ∀ operation
 struct Ds{
     //Data structure
-    int freq[26]={0}; // vector<int> freq(26,0) won't work inside struct
+    int a[26]={0}; // vector<int> freq(26,0) won't work inside struct
     int cost=0; //maintains the curr cost
     //behaves as freq array for every m len window of string S
 
     //methods
     void insert(char ch){    //O(1)
-        cost-=max(0,freq[ch-'a']-const_freq[ch-'a']); //remove the prev contribution of ch from prev ans
-        freq[ch-'a']++;                               //update the freq of ch
-        cost+=max(0,freq[ch-'a']-const_freq[ch-'a']); //add the new contribution to ans
+        cost-=max(0,a[ch-'a']-b[ch-'a']); //remove the prev contribution of ch from prev ans
+        a[ch-'a']++;                               //update the freq of ch
+        cost+=max(0,a[ch-'a']-b[ch-'a']); //add the new contribution to ans
     }
     void erase(char ch){     //O(1)
-        cost-=max(0,freq[ch-'a']-const_freq[ch-'a']); //remove the prev contribution of ch from prev ans
-        freq[ch-'a']--;                               //update the freq of ch
-        cost+=max(0,freq[ch-'a']-const_freq[ch-'a']); //add the new contribution to ans
+        cost-=max(0,a[ch-'a']-b[ch-'a']); //remove the prev contribution of ch from prev ans
+        a[ch-'a']--;                               //update the freq of ch
+        cost+=max(0,a[ch-'a']-b[ch-'a']); //add the new contribution to ans
     }
     int getcost(){          //O(1)
         return cost;
